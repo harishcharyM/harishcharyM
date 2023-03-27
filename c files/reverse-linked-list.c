203f43def546eb4acfb80c36e60aa80450c3ea34
harishcharyM <harishcharymanchoju@gmail.com>
2023-03-27 13:58:42 +0530

    linked list file updated

--- c files/reverse-linked-list.c
+++ c files/reverse-linked-list.c
@@ -3,7 +3,7 @@
                             Online C Compiler.
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-
+//// Need to add more functions do some operations ///
 *******************************************************************************/
 
 #include <stdio.h>
